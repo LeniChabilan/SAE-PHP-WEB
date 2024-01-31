4d64772011cea2708d9b273a356c5986a1344866
LeniChabilan <leni.chabilan@etu.univ-orleans.fr>
2024-01-31 23:17:20 +0100

    Ajout des fichiers pour ajouter musique utilisateur et notes a la BD

--- Modele/NoterModele.php
+++ Modele/NoterModele.php
@@ -7,6 +7,7 @@ $file_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 $file_db->exec("CREATE TABLE IF NOT EXISTS Noter ( 
     albumId INTEGER,
     utilisateurId INTEGER,
+    note INTEGER,
     FOREIGN KEY(albumId) REFERENCES Album(albumId),
     FOREIGN KEY(utilisateurId) REFERENCES Utilisateur(utilisateurId)
     )");
