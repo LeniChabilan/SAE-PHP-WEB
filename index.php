9037e8ed006ddc99e1b3968afd5f20c02fdb4679
Sottier Liam <o22202962@info03-31.iut45.univ-orleans.fr>
2024-02-01 12:10:54 +0100

    lien entre la bd et la page des albums

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <link rel="stylesheet" href="static/base.css" />
+  <link rel="stylesheet" href="static/home.css" />
+  <head>
+    <meta charset="utf-8" />
+    <title>Music'o</title>
+    <script src="https://kit.fontawesome.com/b2318dca58.js" crossorigin="anonymous"></script>
+  </head>
+  <body class="light">
+    
+    <header>
+        <div class="gauche"><a href="">
+            <img class="img_base" id="img1" src="../Data/images/logo.png" alt="image du logo de l'application" />
+          </a>
+          <h2 id="nom_Entreprise">MUSIC'O</h2>
+      </div>
+        
+      <div class="droite">
+        <a href="./connexion.html"><button type="button" id="bouton_se_connecter">Se connecter</button></a>
+        </ul>
+      </div> 
+    </header>
+    <div class="centre">
+    <aside>
+        <div class="d_aside">
+            <ul class="redirection">
+            <li><a href="#" class="btn btn-lg desactiver">
+                <span>Accueil</span>
+            </a></li>
+            <li><a href="#" class="btn btn-lg">
+                <span>Mes playlist</span>
+            </a></li>
+            <li><a href="#" class="btn btn-lg">
+                <span>Mes likes</span>
+            </a></li>
+        </ul>
+    </div>
+        
+    </aside>
+    <main>
+      <form  class="searchBar"  action="#">
+          <input type="text" placeholder="Nom de l'album" >
+          <button type="submit" value="Filtrer">Filtrer</button>
+      </form>
+      <ul class="listeAl">
+        <?php
+            $file_db = new PDO('sqlite:Data/bd.sqlite3');
+            $file_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+            
+            $query = "SELECT * FROM Album";
+            
+            $result = $file_db->query($query);
+            
+            if ($result) {
+                foreach ($result as $row) {
+                    echo "<li>";
+                    echo "<a href='#'>";
+                    echo "<img src='Data/images/".$row['imageAlbum']."'";
+                    echo "<p class='nomAl'>".$row['nomAlbum']."</p>";
+                    echo "</a>";
+                    echo "</li>";
+                }
+            } else {
+                echo "Erreur lors de l'exécution de la requête.";
+            }
+            
+            $file_db = null;
+
+        ?>
+          
+      </ul>
+      
+  </main>
+</div>
+    <footer>
+      <div class="fin"><p>© 2024 MUSIC'O. Tous droits réservés.</p></div>      
+    </footer>
+  </body>
+</html>
