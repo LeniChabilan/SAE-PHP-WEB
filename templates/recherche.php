7a02cc47d1d06d4fcd725662775d6258d1e17316
LeniChabilan <leni.chabilan@etu.univ-orleans.fr>
2024-02-12 21:48:20 +0100

    ajout du css pour le bouton add playlist et correction de la requetede searchbar

--- templates/recherche.php
+++ templates/recherche.php
@@ -7,12 +7,20 @@ $file_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 // Requête SQL pour filtrer les albums en fonction du terme de recherche
 if (is_numeric($searchTerm)) {
     // Si le terme de recherche est numérique, recherchez uniquement par année
-    $query = "SELECT * FROM Album WHERE AnneeAlbum = :searchYear";
+    $query = "SELECT DISTINCT Album.* FROM Album WHERE AnneeAlbum = :searchYear";
     $stmt = $file_db->prepare($query);
     $stmt->bindParam(':searchYear', $searchTerm, PDO::PARAM_INT);
 } else {
     // Si le terme de recherche n'est pas numérique, recherchez par nom d'album ou nom d'artiste
-    $query = "SELECT * FROM Album NATURAL JOIN Artiste NATURAL JOIN GenreAlbum Natural Join Genre WHERE nomAlbum LIKE :searchTerm OR AnneeAlbum LIKE :searchTerm OR nomArtiste LIKE :searchTerm OR nomGenre LIKE :searchTerm";    $stmt = $file_db->prepare($query);
+    $query = "SELECT DISTINCT Album.* FROM Album 
+              NATURAL JOIN Artiste 
+              NATURAL JOIN GenreAlbum 
+              NATURAL JOIN Genre 
+              WHERE nomAlbum LIKE :searchTerm 
+              OR AnneeAlbum LIKE :searchTerm 
+              OR nomArtiste LIKE :searchTerm 
+              OR nomGenre LIKE :searchTerm";
+    $stmt = $file_db->prepare($query);
     $searchTerm = "%$searchTerm%";
     $stmt->bindParam(':searchTerm', $searchTerm, PDO::PARAM_STR);
 }
